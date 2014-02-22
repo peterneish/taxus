287c1953b49cd2c67c3283e0302c4a32728ea0be
Peter Neish <peter.neish@gmail.com>
2014-02-22 23:27:50 +1100

    adding in bloodhoud suggestions

--- app.js
+++ app.js
@@ -1,53 +1,28 @@
 // initialise bloodhound engine
-var engine = new Bloodhound({
+var taxa = new Bloodhound({
 	name: 'taxa',
-	remote: 'http://bie.ala.org.au/search/auto.jsonp?q=%QUERY&limit=10&idxType=TAXON',
-	datumTokenizer: function(d){
-		return Bloodhound.tokenizers.whitespace(d.val);
-	},
-	queryTokenizer: Bloodhound.tokenizers.whitespace
+	datumTokenizer: function (d) {
+        return Bloodhound.tokenizers.whitespace(d.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+	remote: {
+		url: 'http://bie.ala.org.au/search/auto.jsonp?q=%QUERY&limit=10&idxType=TAXON',
+		filter: function(data) {
+
+              var resultList = data.autoCompleteList.map(function (item) {
+              return item.name;
+
+          });
+          console.log(resultList);
+          return resultList;
+
+        }
+	}		
 });
 
-	
+taxa.initialize();
 
-$("#searchvalue").typeahead({
-        minLength: 3,
-		items: 20,
-        source: function(query, process) {
-            $.getJSON('http://bie.ala.org.au/search/auto.jsonp?callback=?',
-		      { q: query, limit: 100, idxType: "TAXON", geoOnly: "true"}, 
-		      function(data) {
-			
-				rows = new Array();
-				mapped = {};
-				$.each(data.autoCompleteList, function(i, name){
-					var query_label = name.matchedNames[0];
-					mapped[query_label] = {rankString: name.rankString, guid: name.guid};
-					
-					rows.push(query_label);
-				});
-				data = data.autoCompleteList;
-				for(var i=0; i<data.length; i++){
-					rows[i] = data[i].matchedNames[0];
-				}
-            	});
-        },
-		updater: function(query_label){
-			var rankString = mapped[query_label].rankString;
-			$("#searchtype").val(rankString);
-			
-			//console.log(rankString + ":" + query_label + ":" + mapped[query_label].guid);
-			$("#splash").hide();
-
-			search_current = rankString;
-			
-			$("#waiting").show();
-	
-			doChart(mapped[query_label].guid, rankString);
-			
-			//$("a.facet").removeClass("disabled");
-			//$("a[href='taxon']").addClass("btn-primary");
-						
-			return query_label;			
-		}
+$("#searchvalue").typeahead(null, {
+    displayKey: 'value',
+    source: taxa.ttAdapter()
 });
