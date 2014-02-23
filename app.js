49ff56069fc511c832e44a11f75178264ea7260e
Peter Neish <peter.neish@gmail.com>
2014-02-23 22:44:13 +1100

    autoselect lists 10 items

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 // initialise bloodhound engine
 var taxa = new Bloodhound({
 	name: 'taxa',
+        limit : 10,
 	datumTokenizer: function (d) {
         return Bloodhound.tokenizers.whitespace(d.value);
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
 	remote: {
-		url: 'http://bie.ala.org.au/search/auto.jsonp?q=%QUERY&limit=10&idxType=TAXON&callback=callback',
+		url: 'http://bie.ala.org.au/search/auto.jsonp?q=%QUERY&limit=20&idxType=TAXON&callback=callback',
 		ajax: {dataType: 'jsonp', jsonpCallback: 'callback'},
 		filter: function(data) {
 
