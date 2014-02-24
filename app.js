75419960c04b071c420996ae64ffa843f535fdf0
Peter Neish <peter.neish@gmail.com>
2014-02-24 14:05:37 +1100

    first setup of viz with simple dots - using jsonp too

--- app.js
+++ app.js
@@ -41,30 +41,80 @@ $("#searchvalue").on("typeahead:selected typeahead:autocompleted", function(e,da
 
 
 // set up d3 stuff
-var diameter = 960,
-    format = d3.format(",d"),
-    color = d3.scale.category20c();
 
-var bubble = d3.layout.pack()
-    .sort(null)
-    .size([diameter, diameter])
-    .padding(1.5);
+var width = 960,
+    height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+    .linkDistance(10)
+    .linkStrength(2)
+    .size([width, height]);
 
 var svg = d3.select("#viz").append("svg")
-    .attr("width", diameter)
-    .attr("height", diameter)
-    .attr("class", "bubble");
+    .attr("width", width)
+    .attr("height", height);
+
+
 
 
 
 // the function that runs the visualisation
 function runViz(guid){
-	speciesurl = "http://bie.ala.org.au/ws/species/" + guid + ".json?callback=?";
+	speciesurl = "http://bie.ala.org.au/ws/species/" + guid + ".json?callback={callback}";
 	$("<a />", {
 		href: speciesurl,
 		text: speciesurl
 		}).appendTo("#viz");
 	
+	d3.jsonp(speciesurl, function(graph) {
+	  var guid = graph.taxonConcept.guid;
+	  var nodes = [];
+	  var  links = [];
+	  
+	  nodes.push({name: graph.taxonConcept.nameString});
+	  
+	  var i=1;
+	  graph.synonyms.forEach(function(link) {
+		links.push({source: 0, target: i++, value: link.relationship});		
+		nodes.push({ name: link.nameString} );
+	  });
+	  
+	  console.log(links);
+	  console.log(nodes);
+
+	  force
+		  .nodes(nodes)
+		  .links(links)
+		  .start();
+
+	  var link = svg.selectAll(".link")
+		  .data(links)
+		.enter().append("line")
+		  .attr("class", "link")
+		  ;
+
+	  var node = svg.selectAll(".node")
+		  .data(nodes)
+		.enter().append("circle")
+		  .attr("class", "node")
+		  .attr("r", 5)
+		  .call(force.drag);
+
+	  node.append("title")
+		  .text(function(d) { return d.name; });
+
+	  force.on("tick", function() {
+		link.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; });
+
+		node.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) { return d.y; });
+	  });
+	});
 }
 
 
