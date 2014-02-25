f872d34f91224ae07efbe51b5513c6bf97e71db5
Peter Neish <peter.neish@gmail.com>
2014-02-25 22:13:08 +1100

    added labels to nodes

--- app.js
+++ app.js
@@ -52,13 +52,14 @@ var w = window,
 var width = Math.max( x * 0.9, 400 ),  //width
     height = Math.max( y * 0.9, 300 ); //height
 var colour = d3.scale.category20();
-var linktype = d3.scale.category20c();
+var linkcolour = d3.scale.category10();
 
 var circlesize = 20;
 
 var force = d3.layout.force()
+    .gravity(.05)
+    .distance(100)
     .charge(-100)
-    .linkDistance(80)
     .size([width, height]);
 
 var svg = d3.select("#viz").append("svg")
@@ -90,8 +91,6 @@ function runViz(guid){
 		nodes.push({ name: link.nameString, guid: link.guid} );
 	  });
 	  
-	  console.log(links);
-	  console.log(nodes);
 
 	  force
 		  .nodes(nodes)
@@ -99,30 +98,44 @@ function runViz(guid){
 		  .start();
 
 	  var link = svg.selectAll(".link")
-		  .data(links)
-		.enter().append("line").
-		 attr("class", "link");
+		   	.data(links)
+			.enter().append("line").
+		 	attr("class", "link").
+		 	attr("stroke-width", 5).
+		 	attr("stroke", function(d) { return linkcolour(d.value); });
 
-	  var node = svg.selectAll(".node")
-		  .data(nodes)
-		.enter().append("circle")
+	  var node = svg.selectAll("g.node")
+		  .data(nodes);
+
+	  node.enter().append("svg:g")
+		.attr("class", "node")
+		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+
+	  node.append("circle")
 		  .attr("class", "node")
 		  .attr("r", 15)
-		  .style("fill", function(d) { return  colour(d.name); })
-		  .call(force.drag);
+		  .style("fill", function(d) { return  colour(d.name); });
 
 	  node.append("title")
 		  .text(function(d) { return d.name; });
 
-	  force.on("tick", function() {
-		link.attr("x1", function(d) { return d.source.x; })
-			.attr("y1", function(d) { return d.source.y; })
-			.attr("x2", function(d) { return d.target.x; })
-			.attr("y2", function(d) { return d.target.y; });
+  	  node.append("text")
+      		.attr("dx", 12)
+      		.attr("dy", ".35em")
+      		.text(function(d) { return d.name });
 
-		node.attr("cx", function(d) { return d.x; })
-			.attr("cy", function(d) { return d.y; });
-	  });
+	  node.call(force.drag);
+	  	
+
+	force.on("tick", function() {
+    		link.attr("x1", function(d) { return d.source.x; })
+        	.attr("y1", function(d) { return d.source.y; })
+        	.attr("x2", function(d) { return d.target.x; })
+        	.attr("y2", function(d) { return d.target.y; });
+
+    		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  	});
 	});
 }
 
